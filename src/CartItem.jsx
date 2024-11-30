368a82a81ff18dc4cd853898d58a60cff795bfa7
Ramioy <yoimar.mb@outlook.com>
2024-11-30 11:19:14 -0500

    title to cooming soon button

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -61,7 +61,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button title='Cooming soon!!' className="get-started-button1">Checkout</button>
       </div>
     </div>
   );
